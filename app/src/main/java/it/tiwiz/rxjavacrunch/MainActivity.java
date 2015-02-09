7a270dcf4bc02dbc6909a013ea9fa55fa39c7470
Roberto Orgiu <roberto.orgiu@gmail.com>
2015-02-09 21:27:22 +0100

    Base project

---
+++ app/src/main/java/it/tiwiz/rxjavacrunch/MainActivity.java
@@ -0,0 +1,47 @@
+package it.tiwiz.rxjavacrunch;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import it.tiwiz.rxjavacrunch.part1.Part1Activity;
+
+
+public class MainActivity extends ActionBarActivity implements View.OnClickListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        wireUi();
+    }
+
+    private void wireUi() {
+        setTapListener(R.id.btnPart1);
+    }
+
+    private void setTapListener(int viewId) {
+        findViewById(viewId).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        startActivity(getLaunchIntentFor(v.getId()));
+    }
+
+    private Intent getLaunchIntentFor(int viewId) {
+        Intent launchIntent = null;
+
+        switch (viewId) {
+            case R.id.btnPart1:
+                launchIntent = new Intent(this, Part1Activity.class);
+                break;
+        }
+
+        return launchIntent;
+    }
+}
---
+++ app/src/main/java/it/tiwiz/rxjavacrunch/part1/Part1Activity.java
@@ -0,0 +1,18 @@
+package it.tiwiz.rxjavacrunch.part1;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import it.tiwiz.rxjavacrunch.R;
+
+public class Part1Activity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_part1);
+    }
+
+}
