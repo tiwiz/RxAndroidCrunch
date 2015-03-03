41448103550141e18f1c0ac1bce0a2157ed3f9e2
Roberto Orgiu <roberto.orgiu@gmail.com>
2015-03-03 23:11:51 +0100

    Adds JavaDoc

--- app/src/main/java/it/tiwiz/rxjavacrunch/part2/Part2Activity.java
+++ app/src/main/java/it/tiwiz/rxjavacrunch/part2/Part2Activity.java
@@ -20,6 +20,10 @@ public class Part2Activity extends ActionBarActivity {
     final String[] manyWords = {"Hello", "to", "everyone", "from", "RxAndroid",
             "something", "that", "is", "really", "nice"};
 
+    /**
+     * We don't need to use a full {@link rx.Subscriber}, we just want to create an
+     * action from when we succeed. 
+     */
     Action1<String> textViewOnNextAction = new Action1<String>() {
         @Override
         public void call(String s) {
@@ -34,6 +38,10 @@ public class Part2Activity extends ActionBarActivity {
         }
     };
 
+    /**
+     * Creates a method that will be executed after the {@link rx.Observable}
+     * and will return a String to the {@link rx.Subscriber}
+     */
     Func1<String, String> toUpperCaseMap = new Func1<String, String>() {
         @Override
         public String call(String s) {
@@ -49,12 +57,18 @@ public class Part2Activity extends ActionBarActivity {
         txtPart1 = (TextView) findViewById(R.id.part1text);
         context = this;
 
+        /**
+         * Creates an {@link rx.Observable} that will be emitted only once 
+         */
         Observable<String> singleObservable = Observable.just("Hello, World!");
 
         singleObservable.observeOn(AndroidSchedulers.mainThread())
                 .map(toUpperCaseMap)
                 .subscribe(textViewOnNextAction);
 
+        /**
+         * Emits one item at the time, taking them from any {@link java.util.Collection} 
+         */
         Observable<String> oneByOneObservable = Observable.from(manyWords);
         oneByOneObservable.observeOn(AndroidSchedulers.mainThread())
                 .subscribe(toastOnNextAction);
