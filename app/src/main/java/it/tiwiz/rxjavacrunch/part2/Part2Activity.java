952ece6e6e2c26ba51f092030f052444982e8711
Roberto Orgiu <roberto.orgiu@gmail.com>
2015-03-03 23:08:35 +0100

    Adds example with from() with array

--- app/src/main/java/it/tiwiz/rxjavacrunch/part2/Part2Activity.java
+++ app/src/main/java/it/tiwiz/rxjavacrunch/part2/Part2Activity.java
@@ -8,9 +8,7 @@ import android.widget.Toast;
 
 import it.tiwiz.rxjavacrunch.R;
 import rx.Observable;
-import rx.Subscriber;
 import rx.android.schedulers.AndroidSchedulers;
-import rx.functions.Action;
 import rx.functions.Action1;
 import rx.functions.Func1;
 
@@ -19,28 +17,30 @@ public class Part2Activity extends ActionBarActivity {
     TextView txtPart1;
     Context context;
 
-    
+    final String[] manyWords = {"Hello", "to", "everyone", "from", "RxAndroid",
+            "something", "that", "is", "really", "nice"};
+
     Action1<String> textViewOnNextAction = new Action1<String>() {
         @Override
         public void call(String s) {
             txtPart1.setText(s);
         }
     };
-    
+
     Action1<String> toastOnNextAction = new Action1<String>() {
         @Override
         public void call(String s) {
             Toast.makeText(context, s, Toast.LENGTH_SHORT).show();
         }
     };
-    
+
     Func1<String, String> toUpperCaseMap = new Func1<String, String>() {
         @Override
         public String call(String s) {
             return s.toUpperCase();
         }
     };
-    
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -49,16 +49,16 @@ public class Part2Activity extends ActionBarActivity {
         txtPart1 = (TextView) findViewById(R.id.part1text);
         context = this;
 
-        Observable<String> observable = Observable.just("Hello, World!");
+        Observable<String> singleObservable = Observable.just("Hello, World!");
 
-        observable.observeOn(AndroidSchedulers.mainThread())
+        singleObservable.observeOn(AndroidSchedulers.mainThread())
                 .map(toUpperCaseMap)
                 .subscribe(textViewOnNextAction);
-        observable.subscribe(toastOnNextAction);
+
+        Observable<String> oneByOneObservable = Observable.from(manyWords);
+        oneByOneObservable.observeOn(AndroidSchedulers.mainThread())
+                .subscribe(toastOnNextAction);
     }
-
-
-
 
 
 }
