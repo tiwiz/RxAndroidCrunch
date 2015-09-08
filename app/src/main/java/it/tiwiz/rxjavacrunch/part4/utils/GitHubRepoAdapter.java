04d3e39423fc2dc66db75b17ff8eea0e9a795b54
Roberto Orgiu <roberto.orgiu@gmail.com>
2015-09-08 18:02:15 +0200

    Inits repo adapter

--- app/src/main/java/it/tiwiz/rxjavacrunch/part4/utils/GitHubRepoAdapter.java
+++ app/src/main/java/it/tiwiz/rxjavacrunch/part4/utils/GitHubRepoAdapter.java
@@ -1,7 +1,24 @@
 package it.tiwiz.rxjavacrunch.part4.utils;
 
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.TextView;
+
+import it.tiwiz.rxjavacrunch.R;
+
 /**
- * Created by roberto on 08/09/15.
+ *
  */
 public class GitHubRepoAdapter {
+
+    public static class RepoHolder extends RecyclerView.ViewHolder {
+        private final TextView firstLine;
+        private final TextView secondLine;
+
+        public RepoHolder(View itemView) {
+            super(itemView);
+            firstLine = (TextView) itemView.findViewById(R.id.firstLine);
+            secondLine = (TextView) itemView.findViewById(R.id.secondLine);
+        }
+    }
 }
