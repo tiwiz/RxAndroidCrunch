a1a95f997ac5dce8fb18a1c5d37ce8d8fb489a9c
Roberto Orgiu <roberto.orgiu@gmail.com>
2015-10-20 07:50:09 +0200

    Wires up Activity components

--- app/src/main/java/it/tiwiz/rxjavacrunch/part5/Part5Activity.java
+++ app/src/main/java/it/tiwiz/rxjavacrunch/part5/Part5Activity.java
@@ -6,16 +6,44 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.TextView;
 
 import it.tiwiz.rxjavacrunch.R;
 
-public class Part5Activity extends AppCompatActivity {
+public class Part5Activity extends AppCompatActivity implements View.OnClickListener{
+
+    private TextView textEmittedNumber, textFragmentStatus;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_part5);
+        wireUpActivity();
+    }
+
+    private void wireUpActivity() {
+        textEmittedNumber = (TextView) findViewById(R.id.mainEmittedNumber);
+        textFragmentStatus = (TextView) findViewById(R.id.fragmentStatus);
+        findViewById(R.id.btnShowFragment).setOnClickListener(this);
+        findViewById(R.id.btnHideFragment).setOnClickListener(this);
     }
 
 
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.btnShowFragment) {
+            addFragment();
+        } else {
+            removeFragment();
+        }
+    }
+
+    private void addFragment() {
+
+    }
+
+    private void removeFragment() {
+
+    }
 }
