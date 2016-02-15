f672d87c40d161cf8ed1b510ecf52775413fe344
Roberto Orgiu <roberto.orgiu@gmail.com>
2016-02-15 11:19:21 +0100

    Starts implementing Operators

--- app/src/main/java/it/tiwiz/rxjavacrunch/part7/Part7Activity.java
+++ app/src/main/java/it/tiwiz/rxjavacrunch/part7/Part7Activity.java
@@ -5,28 +5,53 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.jakewharton.rxbinding.widget.RxTextView;
 
 import it.tiwiz.rxjavacrunch.R;
+import rx.functions.Action1;
 
 public class Part7Activity extends AppCompatActivity {
+
+    FloatingActionButton fab;
+    TextView txtResponse;
+    EditText txtNumber;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_part7);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        bindToolbar();
+        bindFab();
+        bindTextViews();
+    }
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+    private void bindToolbar() {
+        setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    private void bindFab() {
+        fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(this::onCalculationRequest);
+    }
+
+    private void bindTextViews() {
+        txtNumber = (EditText) findViewById(R.id.txtNumber);
+        RxTextView.textChanges(txtNumber).subscribe(this::onNumberStatusInserted);
+        txtResponse = (TextView) findViewById(R.id.txtResponse);
+    }
+
+    public void onCalculationRequest(View view) {
+
+    }
+
+    public void onNumberStatusInserted(CharSequence charSequence) {
+        fab.setEnabled(!TextUtils.isEmpty(charSequence));
     }
 
 }
