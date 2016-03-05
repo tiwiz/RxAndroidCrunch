90892686559a94d66a867a5db61bc0d646b5e510
Roberto Orgiu <roberto.orgiu@gmail.com>
2016-03-05 17:49:52 +0100

    Prepares boilerplate for the new part

---
+++ app/src/main/java/it/tiwiz/rxjavacrunch/part8/Part8Activity.java
@@ -0,0 +1,15 @@
+package it.tiwiz.rxjavacrunch.part8;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import it.tiwiz.rxjavacrunch.R;
+
+public class Part8Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_part8);
+    }
+}
